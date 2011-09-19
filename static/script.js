4734cca5908dc1c8307590142e3f6b5452baeb08
Sam White <brokensambot@gmail.com>
2011-09-18 23:33:38 -0400

    added MIT license

--- static/script.js
+++ static/script.js
@@ -1,9 +1,36 @@
+// Copyright (c) 2011 Sam White
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+var answersLimit;
+var timer;
+
 $(document).ready(function() {
     $('#settings').click(showMenu);
     $('#access').click(clearAccess);
     $('#secret').click(clearSecret);
     $('#access').focusout(fillAccess);
     $('#secret').focusout(fillSecret);
+    $('#search').click(startSearch);
+    $('#lucky').click(startSearch);
 });
 
 function showMenu(e) {
@@ -15,7 +42,7 @@ function showMenu(e) {
                         'right':'4px'});
     $('#menu').css({'visibility':'visible'});
     $('#settings').unbind();
-    setTimeout(function() {$(document).click(hideMenu)}, 0);
+    setTimeout(function() {$(document).click(hideMenu);}, 0);
 }
 
 function hideMenu(e) {
@@ -32,7 +59,7 @@ function hideMenu(e) {
                         'right':'5px'});
     $('#menu').css({'visibility':'hidden'});
     $(document).unbind();
-    setTimeout(function() {$('#settings').click(showMenu)}, 0);
+    setTimeout(function() {$('#settings').click(showMenu);}, 0);
 }
 
 function clearAccess(e) {
@@ -65,4 +92,38 @@ function fillSecret(e) {
         node.val('AWS Secret Key');
         node.css({'color':'#c0c0c0'});
     }
+}
+
+function startSearch(e) {
+    clearInterval(timer);
+    $('.answer').fadeOut(100, function() {$(this).remove();});
+    if (e.target == $('#lucky')[0])
+        answersLimit = 1;
+    else
+        answersLimit = 10;
+    postQuestion();
+    timer = setInterval(function() {updateSearch();}, 5000);
+}
+
+function updateSearch() {
+    var answers = getAnswers();
+    if (answers.length == answersLimit) {
+        clearInterval(timer);
+    }
+    for (var i = $('.answer').length; i < answers.length; i++) {
+        var node = $('<p class="answer">' + answers[i] + '</p>');
+        $('body').append(node.hide().fadeIn(100));
+    }
+}
+
+function postQuestion() {
+    /* Post question here. */
+}
+
+function getAnswers() {
+    /* Get answers here. */
+    
+    /* Begin testing. */
+    return ['White', 'Pink', 'Red', 'Orange', 'Brown', 'Yellow', 'Gray', 'Green', 'Cyan', 'Blue'];
+    /* End testing. */
 }
