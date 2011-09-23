349e1b7e06298dff9c60e6efd036156e936967e9
Sam White <brokensambot@gmail.com>
2011-09-22 22:40:05 -0400

    minor yaml changes

--- api.py
+++ api.py
@@ -22,33 +22,64 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
+from google.appengine.ext import db
 from boto.mturk.question import ExternalQuestion
 from boto.mturk.connection import MTurkConnection
 from google.appengine.ext.webapp.util import run_wsgi_app
 from django.utils import simplejson
 from google.appengine.ext import webapp
 
+class Question(db.Model):
+    date = db.DateTimeProperty(auto_now_add=True)
+    text = db.TextProperty()
+
 class QuestionHandler(webapp.RequestHandler):
     # This URL should not handle GET requests.
     def get(self):
-        self.error(404)
+        self.error(405)
     
     def post(self):
+        accessKey = self.request.get(argument_name='accessKey', default_value=None)
+        secretKey = self.request.get(argument_name='secretKey', default_value=None)
+        answers = self.request.get(argument_name='answers', default_value=None)
+        text = self.request.get(argument_name='text', default_value=None)
+        
+        if accessKey == None or secretKey == None or answers == None or text == None:
+            self.error(400)
+        
+        question = Question(text=text)
+        question.put()
+        
+        # FIXME: Send the question to Mechanical Turk.
+        
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write(simplejson.dumps({'questionId': 12345}, indent=4))
+        self.response.out.write(simplejson.dumps({'questionId': 1}, indent=4))
 
 class AnswersHandler(webapp.RequestHandler):
     def get(self):
+        # FIXME: Get the answers from Mechanical Turk.
+        
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(simplejson.dumps({'answers': ['Red', 'Green', 'Blue']}, indent=4))
     
     # This URL should not handle POST requests.
     def post(self):
-        self.error(404)
+        self.error(405)
+
+class HITHandler(webapp.RequestHandler):
+    def get(self):
+        questionId = self.request.get('questionId')
+    
+    # FIXME: Serve the HIT to Mechanical Turk.
+    
+    # This URL should not handle POST requests.
+    def post(self):
+        self.error(405)
 
 def main():
     run_wsgi_app(webapp.WSGIApplication([('/api/question', QuestionHandler),
-                                         ('/api/answers', AnswersHandler)],
+                                         ('/api/answers', AnswersHandler),
+                                         ('/api/hit', HITHandler)],
                                         debug=True))
 
 if __name__ == '__main__':
